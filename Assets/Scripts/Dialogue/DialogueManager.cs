9606be4d5d680c22e160b6583a47c3e9c6b7d4a6
Akshaj <akshaj.kanumuri@gmail.com>
2022-12-30 16:59:29 -0800

    update

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator anim;
+
+    public Queue<string> sentences;
+
+    private void Start()
     {
-        
+        sentences= new Queue<string>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void StartDialogue(Dialogue dialogue)
     {
-        
+        anim.SetBool("IsOpen", true);
+
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count  == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("Dialogue has ended");
+        anim.SetBool("IsOpen", false);
     }
 }
